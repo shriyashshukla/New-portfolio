688e2a934d208f65e046af00a9ad36d622047011
Shriyash shukla <139564061+shriyashshukla@users.noreply.github.com>
2024-08-09 22:34:17 +0530

    chore: Remove unused code and fix formatting in TestimonialCards component

--- src/components/Live.tsx
+++ src/components/Live.tsx
@@ -22,7 +22,7 @@ export function Live() {
       </motion.h1>
      
     </LampContainer>
-    <MusicSchoolTestimonials/>
+   
     </>
 
   );
--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -36,7 +36,7 @@ const musicSchoolTestimonials = [
 
 function MusicSchoolTestimonials() {
   return (
-    <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+    <div className="h-[40rem] w-full relative flex flex-col items-center justify-center overflow-hidden bg-transparent">
         <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
         <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
             <div className="w-full max-w-6xl">
@@ -51,4 +51,4 @@ function MusicSchoolTestimonials() {
   )
 }
 
-export default MusicSchoolTestimonials
+export default MusicSchoolTestimonials;
